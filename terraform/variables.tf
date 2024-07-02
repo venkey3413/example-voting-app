1b68abea63156c92520e00a059864193494d7ff4
venkey3413 <144769456+venkey3413@users.noreply.github.com>
2024-07-02 12:21:16 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,27 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "region" {
-  default = "us-west-2"
+variable "aws_access_key" {
+  description = "AKIAXYKJQKV6UT2WM4NR"
 }
+
+variable "aws_secret_key" {
+  description = "WlEbNX07oF9KFYa0wHy4zCiXikLxCgEKcEw/k4Y6"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
 variable "instance_type" {
   default = "t2.micro"
 }
+
 variable "ami_id" {
   default = "ami-0c55b159cbfafe1f0"  # Replace with your preferred AMI ID
 }
+
 variable "key_name" {
   description = "key"
+}
+
+variable "key_path" {
+  description = "key_path = "~/.ssh/key.pem""
 }
