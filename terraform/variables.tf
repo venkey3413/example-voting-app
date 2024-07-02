c34d00de83d5cc1faa32694f5b68d83b12c8422a
venkey3413 <144769456+venkey3413@users.noreply.github.com>
2024-07-02 11:57:32 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+  default = "us-west-2"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ami_id" {
+  default = "ami-0c55b159cbfafe1f0"  # Replace with your preferred AMI ID
+}
+variable "key_name" {
+  description = "Name of the key pair"
+}
